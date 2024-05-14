fd55a22d47f337a72384f0a57b85ad6c13d3787d
MandemGibson <themaingib@gmail>
2024-05-14 20:24:38 +0000

    Product Details Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from "react-native";
 import ProductsScreen from "@/src/screens/ProductsScreen";
+import ProductDetailsScreen from "@/src/screens/ProductDetailsScreen";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <ProductsScreen/>
+      {/* <ProductsScreen/> */}
+      <ProductDetailsScreen/>
     </View>
   );
 }
@@ -12,8 +14,6 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
  
 });
